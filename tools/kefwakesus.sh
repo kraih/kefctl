0b8b9d314b4e17f5a24cd5208c49236b51da4f59
Alex <adkipnis@mailbox.org>
2021-11-01 16:59:06 +0100

    changed flag names from -b to -w resp. -B to -W

--- tools/kefwakesus.sh
+++ tools/kefwakesus.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ "${1}" == "pre" ]; then
-  sudo -u username /bin/perl /path/to/kefctl -B
+  sudo -u username /bin/perl /path/to/kefctl -W
 
 elif [ "${1}" == "post" ]; then
-  sudo -u username /bin/perl /path/to/kefctl -b
+  sudo -u username /bin/perl /path/to/kefctl -w
 fi
